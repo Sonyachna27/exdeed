fd66c8b1a8bcc68e182b6687e186b58402d0e767
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2025-07-15 22:50:43 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+		accordionFunction();
+});
+
+const accordionFunction = () => {
+  const accordionItems = document.querySelectorAll(".accord-item");
+  accordionItems.forEach((item) => {
+		const top = item.querySelector(".accord-item-top");
+		if(top){
+			top.addEventListener("click", function () {
+				item.classList.toggle("active");
+			});
+		}
+  });
+};
