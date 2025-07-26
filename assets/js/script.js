f4e6f4a4f2c10dccad12ae17e225a989734c396f
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2025-07-26 23:04:23 +0300

    add style

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 		accordionFunction();
 		presentationSlider();
+		openTabs();
 });
 
 const accordionFunction = () => {
@@ -37,5 +38,57 @@ const presentationSlider = () => {
         prevEl: presentationItem.querySelector('.presentation-button-prev'),
       },
     });
+  });
+};
+
+const openTabs = () => {
+  const tabGroups = document.querySelectorAll(".target__wrap"); 
+
+  tabGroups.forEach((group) => {
+    const tabsLinks = group.querySelectorAll(".target__list-item");
+    const allContentBlocks = group.querySelectorAll(".target__content");
+    let frontBlockId = tabsLinks[0].dataset.name; 
+
+    function addTabsActive() {
+      tabsLinks.forEach((button, index) => {
+        button.addEventListener("click", () => {
+          tabsLinks.forEach((otherButton) => {
+            otherButton.classList.remove("active");
+          });
+          button.classList.add("active");
+          showContent(button.dataset.name, index);
+        });
+      });
+    }
+
+    function updateActiveTab(index) {
+      tabsLinks.forEach((button, i) => {
+        if (i === index) {
+          button.classList.add("active");
+        } else {
+          button.classList.remove("active");
+        }
+      });
+    }
+
+    function changeSlide(blockId) {
+      allContentBlocks.forEach((block) => {
+        if (block.getAttribute("id") === blockId) {
+          block.style.display = "flex";
+          block.style.opacity = 1;
+        } else {
+          block.style.opacity = 0;
+          block.style.display = "none";
+        }
+      });
+      frontBlockId = blockId;
+    }
+
+    function showContent(itemName, index) {
+      changeSlide(itemName);
+      updateActiveTab(index);
+    }
+    addTabsActive();
+    showContent(frontBlockId, 0); 
   });
 };
